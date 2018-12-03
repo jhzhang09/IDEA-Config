0ebf423b789b2bcf7d0c2157363b45614b6fc03c
zhang <zhang@PC-JHZHANG>
2018-12-03 17:59:57 +0800

    IU-2018.3 <zhang@PC-JHZHANG Create vim_settings.xml, ide.general.xml, _@user_Default.icls, ide-features-trainer.xml, project.default.xml, rainbow_brackets.xml, Enum.java, Default copy.xml, Interface.java, lombok-plugin.xml, other.xml, Default.xml, file.template.settings.xml, code.style.schemes, ui.lnf.xml, filetypes.xml, keymap.xml, Solarized Light (Darcula).icls, colors.scheme.xml, _@user_Material Oceanic.icls, mavenVersion.xml, AnnotationType.java, javarebel.xml, _@user_Material Solarized Dark.icls, Default.xml, laf.xml, KeyPromoterXStatistic.xml, material_custom_theme.xml, Class.java, jdk.table.xml, path.macros.xml, mavenRunHelper.xml, Solarized Dark.icls, Solarized Dark (Darcula).icls, material_theme.xml, editor.codeinsight.xml, grepConsole.xml, Solarized Light.icls, editor.xml, debugger.xml, sonarlint.xml, updates.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @author zjh
+ * @date ${DATE} ${TIME}
+ *
+ */
+public interface ${NAME} {
+}
