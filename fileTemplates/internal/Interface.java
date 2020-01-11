8f9350f35617d8ec6c5133d7e5a204c2ab342c1b
jhzhang09 <zhangjh09024@gmail.com>
2020-01-11 18:24:39 +0800

    IU-2019.3.1 <zhangjinhu@zhangjinhu-MacBookPro Create project.default.xml, Enum.java, Interface.java, power.mode.ii.xml, Default.xml, ignore.xml, find.xml, checkstyle-idea.xml, KeyPromoterXSettings.xml, _@user_Darcula.icls, path.macros.xml, mavenVersion.xml, kite.settings.xml, _@user_Material Solarized Dark.icls, GrepConsole.xml, Default.xml, scala.xml, _@user_Material Darker.icls, markdown_navigator_enh.shared.xml, MyBatisCodeHelper.xml, keymap.xml, OGNL.xml, editor.xml, applicationLibraries.xml, markdown_navigator.shared.xml, debugger.xml, updates.xml, baseRefactoring.xml, vim_settings.xml, alibaba-toolkit-tips.xml, ide.general.xml, _@user_Default.icls, ide-features-trainer.xml, rainbow_brackets.xml, lombok-plugin.xml, git.xml, androidStudioFirstRun.xml, code.style.schemes, ui.lnf.xml, gradle.settings.xml, filetypes.xml, projectView.xml, colors.scheme.xml, scala_config.xml, databaseDrivers.xml, github.xml, AnnotationType.java, stringManipulation.xml, OGNL _Struts 2_.xml, material_custom_theme.xml, Class.java, jdk.table.xml, mavenRunHelper.xml, laf.xml, material_theme.xml, vcs.xml, codota.xml, databaseSettings.xml, docker-registry.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * TODO
+ * @author ${USER}
+ * @version 0.1
+ * @date ${DATE} ${TIME}
+ */
+public interface ${NAME} {
+}
